2afc3a0b82b2e95708a58088a5908df2871e878f
Shwetal1805200 <160219881+Shwetal1805200@users.noreply.github.com>
2024-06-08 03:20:47 +0530

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const MONGODB_URL= "mongodb://localhost:27017" ;
+
+exports.connect = () => {
+  mongoose
+    .connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("DB CONNECTED SUCCESSFULLY"))
+    .catch((err) => {
+      console.log("DB CONNECTION FAILED");
+      console.log(err);
+      process.exit(1);
+    });
+};
